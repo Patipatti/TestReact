d247f7743622b433db9d113a20eaf599e9c3479d
Patipatti <23.11.pb@gmail.com>
2025-10-16 19:12:19 +0700

    edit wording

--- test/src/App.js
+++ test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/React</code> and save to reload.
+          Edit <code>src/React</code> start learnning.
         </p>
         <a
           className="App-link"
